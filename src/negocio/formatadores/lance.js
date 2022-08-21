6330cb6d7423752326b00d838b1d72b223a101d5
gabrielSanovis <9novis9@gmail.com>
2022-08-20 22:19:59 -0300

    coverage atualizado

--- src/negocio/formatadores/lance.js
+++ src/negocio/formatadores/lance.js
@@ -1,7 +1,4 @@
 export function formataMaiorLanceDoLeilao(lances, valorInicial) {
-  const maiorLance = lances.reduce(
-    (maior, atual) => atual.valor > maior ? atual.valor : maior,
-    valorInicial
-  );
+  const maiorLance = lances.reduce((maior, atual) => atual.valor > maior ? atual.valor : maior, valorInicial);
   return maiorLance;
 }
